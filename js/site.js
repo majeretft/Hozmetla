c174e85f3044bdb1897b2460c54f8e70c5053642
Andrey Bliznev <majeretft@mail.ru>
2016-01-19 12:45:24 +0300

    Using bootstrap and JQuery from yandex CDN. Moved to JQuery 1.12.0 Recovered site js and carousel.

--- js/site.js
+++ js/site.js
@@ -1,1 +1,36 @@
-﻿
+var CarouselController = function (carousel, thumbnails) {
+	this._carousel = carousel;
+	this._thumbnails = thumbnails;
+};
+
+CarouselController.prototype = {
+	constructor: CarouselController,
+
+	registerEvent: function () {
+		var me = this;
+		$(this._thumbnails).on("click", function (event) {
+			var targetSlide = $(event.currentTarget).attr('data-slide');
+			$(me._carousel).carousel(+targetSlide);
+
+			event.preventDefault ? event.preventDefault() : event.returnValue = false;
+			if (event.stopPropagation)
+				event.stopPropagation();
+		});
+
+		$(this._carousel).on("slide.bs.carousel", function (event) {
+			var nextSlide = $(event.relatedTarget).index();
+			$(me._thumbnails).removeClass("active");
+			$(me._thumbnails[nextSlide]).addClass("active");
+		})
+	}
+};
+
+$(function () {
+	var carousel = $("#photoes")[0];
+	var thumbnails = $(".photoes-thumbnails > li");
+	if (!carousel || thumbnails.length < 1)
+		return;
+
+	var carouselController = new CarouselController(carousel, thumbnails);
+	carouselController.registerEvent();
+});
